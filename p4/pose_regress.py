e602f04889014f580eeb7aa5e3abbf9d61671bef
Ajay Sharma <ajsharm@microsoft.com>
2021-11-29 22:19:48 -0800

    fix

--- p4/pose_regress.py
+++ p4/pose_regress.py
@@ -118,7 +118,8 @@ def setup_xforms_and_data_loaders(par):
         call setup_data_loaders(par) defined in data_io.data_loaders
         It returns a tuple (train loader, test loader)
         """
-        return (None, None) # for now
+        train_loader, test_loader = data_io.data_loaders.setup_data_loaders(par)
+        #return (None, None) # for now
 
     return (train_loader, test_loader)
 
